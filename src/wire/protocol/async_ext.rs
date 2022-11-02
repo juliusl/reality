2c1733c979a9b2a4b53d75fe3d3899be03b3f0e6
Julius Liu <juliusl@microsoft.com>
2022-11-01 22:56:56 -0700

    Feat. refactored async_ext - Made stream opening lazy

--- src/wire/protocol/async_ext.rs
+++ src/wire/protocol/async_ext.rs
@@ -1,3 +1,5 @@
+use std::future::Future;
+
 use tokio::io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt};
 
 use crate::wire::{ControlDevice, Encoder, Frame, WireObject};
@@ -26,15 +28,17 @@ impl Protocol {
 
     /// Async version of Protocol::send
     ///
-    pub async fn send_async<W, Writer>(
+    pub async fn send_async<W, Writer, F>(
         &mut self,
-        control_stream: &mut Writer,
-        frame_stream: &mut Writer,
-        blob_stream: &mut Writer,
+        control_stream: impl FnOnce() -> F,
+        frame_stream: impl FnOnce() -> F,
+        blob_stream: impl FnOnce() -> F,
     ) where
         W: WireObject,
         Writer: AsyncWrite + Unpin,
+        F: Future<Output = Writer>,
     {
+        let mut control_stream = control_stream().await;
         let control_device = ControlDevice::new(self.ensure_encoder::<W>().interner.clone());
         for f in control_device.data_frames() {
             assert_eq!(control_stream.write(f.bytes()).await.ok(), Some(64))
@@ -48,15 +52,17 @@ impl Protocol {
             assert_eq!(control_stream.write(f.bytes()).await.ok(), Some(64));
         }
 
+        let mut frame_stream = frame_stream().await;
         for f in self.ensure_encoder::<W>().frames_slice() {
             assert_eq!(frame_stream.write(f.bytes()).await.ok(), Some(64));
         }
 
         self.ensure_encoder::<W>().blob_device.set_position(0);
 
+        let mut blob_stream = blob_stream().await;
         let blob_len = self.ensure_encoder::<W>().blob_device.get_ref().len();
         assert_eq!(
-            tokio::io::copy(&mut self.ensure_encoder::<W>().blob_device, blob_stream)
+            tokio::io::copy(&mut self.ensure_encoder::<W>().blob_device, &mut blob_stream)
                 .await
                 .ok(),
             Some(blob_len as u64)
@@ -65,17 +71,18 @@ impl Protocol {
 
     /// Async version of Protocol receive
     ///
-    pub async fn receive_async<W, Reader>(
+    pub async fn receive_async<W, Reader, F>(
         &mut self,
-        control_stream: &mut Reader,
-        frame_stream: &mut Reader,
-        blob_stream: &mut Reader,
+        control_stream: impl FnOnce() -> F,
+        frame_stream: impl FnOnce() -> F,
+        blob_stream: impl FnOnce() -> F,
     ) where
         W: WireObject,
         Reader: AsyncRead + Unpin,
+        F: Future<Output = Reader>,
     {
+        let mut control_stream = control_stream().await;
         let mut control_device = ControlDevice::default();
-
         let mut frame_buffer = [0; 64];
         while let Ok(64) = control_stream.read_exact(&mut frame_buffer).await {
             let frame = Frame::from(frame_buffer);
@@ -95,10 +102,12 @@ impl Protocol {
         }
         self.ensure_encoder::<W>().interner = control_device.into();
 
-        tokio::io::copy(blob_stream, &mut self.ensure_encoder::<W>().blob_device)
+        let mut blob_stream = blob_stream().await;
+        tokio::io::copy(&mut blob_stream, &mut self.ensure_encoder::<W>().blob_device)
             .await
             .ok();
 
+        let mut frame_stream = frame_stream().await;
         while let Ok(64) = frame_stream.read_exact(&mut frame_buffer).await {
             let frame = Frame::from(frame_buffer);
             self.ensure_encoder::<W>().frames.push(frame);
