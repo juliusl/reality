7eaaa736b187a8e7c9cd2f56e14ccce936bb42e6
Julius Liu <juliusl@microsoft.com>
2022-11-23 14:57:11 -0800

    Feat. fix blob client trait

--- src/wire/blob_client.rs
+++ src/wire/blob_client.rs
@@ -4,9 +4,11 @@ use tokio::io::AsyncRead;
 /// Trait to enable different blob client implementations downstream,
 /// 
 pub trait BlobClient {
+    /// An example stream type could be DuplexStream,
+    /// 
+    type Stream: AsyncRead + Unpin;
+
     /// Returns a stream to read a range of bytes,
     /// 
-    fn stream_range<S>(&self, range: Range<usize>) -> S
-    where
-        S: AsyncRead + Unpin;
+    fn stream_range(&self, range: Range<usize>) -> Self::Stream;
 }
