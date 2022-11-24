664d94d934dadf58ca8c90140b223efeb046bf3b
Julius Liu <juliusl@microsoft.com>
2022-11-24 15:55:01 -0800

    Feat. adding a trait to abstract block building

--- src/wire/block_store.rs
+++ src/wire/block_store.rs
@@ -43,15 +43,45 @@ pub trait BlockStoreBuilder {
     /// 
     type Store: BlockStore;
 
+    /// The type of block builder,
+    /// 
+    type Builder: BlockBuilder;
+
     /// Includes interner w/ this block store,
     /// 
     fn include_interner(&mut self, interner: &Interner);
 
     /// Returns a join handle, whose result is the result of putting a block in the store,
     /// 
-    fn put_block(&mut self, frame: &Frame, blob: impl Into<Bytes>) -> PutBlock;
+    fn build_block(&mut self, name: impl AsRef<str>) -> &mut Self::Builder;
 
     /// Returns a join handle, whose result is the completed store, 
     /// 
     fn finish(&mut self) -> FinishStore<Self::Store>;
+}
+
+/// Trait that abstracts building a single block in a block store,
+/// 
+pub trait BlockBuilder {
+    /// Returns the name of this block,
+    /// 
+    fn name(&self) -> &String; 
+
+    /// Puts a frame into the frame block data,
+    /// 
+    /// If the frame is an extent, then put_block should be called instead,
+    /// 
+    fn put_frame(&mut self, frame: &Frame);
+
+    /// Returns a join handle, whose result is the result of putting a block in the store,
+    /// 
+    fn put_block(&mut self, frame: &Frame, blob: impl Into<Bytes>) -> PutBlock;
+
+    /// Returns all frames as block data, 
+    /// 
+    fn frame_block_data(&self) -> Bytes; 
+
+    /// Returns an ordered list of frames, which are stored as blocks,
+    /// 
+    fn ordered_block_list(&self) -> Vec<Frame>;
 }
