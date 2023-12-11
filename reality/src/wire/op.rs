7c96d1516cc3fabc3b65590608433b3d4f32760d
Julius Liu <juliusl@microsoft.com>
2023-12-11 15:13:22 -0800

    todo. sketch ops

--- reality/src/wire/op.rs
+++ reality/src/wire/op.rs
@@ -62,37 +62,94 @@
 #[derive(Copy, Hash, PartialEq, PartialOrd, Clone, Eq, Ord, Debug, Default)]
 pub struct Op(uuid::Uuid);
 
-impl Op {
-    /// Returns the current instruction mode,
-    ///
-    pub fn _instruction(self) -> Instruction {
-        let (_, i, _, _) = self.0.as_fields();
-
-        Instruction::from_bits_truncate(i)
-    }
-
-    /// Returns the current operation mode,
-    ///
-    pub fn _mode(self) -> Mode {
-        let (_, _, m, _) = self.0.as_fields();
-
-        Mode::from_bits_truncate(m)
-    }
-}
-
 bitflags::bitflags! {
-    /// Instruction bit flags,
-    ///
-    pub struct Instruction: u16 {
-        const Insert = 1;
-        const Index = 1 << 1;
-        const Reserve = 1 << 2;
-        const Health = 1 << 3;
-    }
+    /// Code-set, consists of 16-bit operations w/ 16-bit modes each.
+    /// 
+    /// Code-set maps to actual language verbs.
+    /// 
+    /// # Language Definition:
+    /// 
+    /// | op     |    mode    | description |
+    /// | ------ | ---------- | ---------------------------- |
+    /// | Op0    |            | Load operations              |
+    /// | ------ | ---------- | ---------------------------- |
+    /// | Op0    | Mode0      | Replace value                |
+    /// | Op0    | Mode1      | Parse value                  |
+    /// | Op0    | Mode2      | Merge value                  |
+    /// | Op0    | Mode3      | Append value                 |
+    /// | ------ | ---------- | ---------------------------- |
+    /// | Op1    |            | Read operation               |
+    /// | ------ | ---------- | ---------------------------- |
+    /// | Op1    | Mode0      | bincode -> UTF-8 String      |
+    /// | Op1    | Mode1      | bincode -> Decorated<String> |
+    /// | ------ | ---------- | ---------------------------- |
+    /// | Op2    |            | Util operations              |
+    /// | ------ | ---------- | ---------------------------- |
+    /// 
+    /// 
+    pub struct Code: u32 {
+        const Op0 = 0;
+        const Op1 = 1 << 1;
+        const Op2 = 1 << 2;
+        const Op3 = 1 << 3;
+        const Op4 = 1 << 4;
+        const Op5 = 1 << 5;
+        const Op6 = 1 << 6;
+        const Op7 = 1 << 7;
+        const Op8 = 1 << 8;
+        const Op9 = 1 << 9;
+        const OpA = 1 << 10;
+        const OpB = 1 << 11;
+        const OpC = 1 << 12;
+        const OpD = 1 << 13;
+        const OpE = 1 << 14;
+        const OpF = 1 << 15;
+        const Mode0 = 1 << 16;
+        const Mode1 = 1 << 17;
+        const Mode2 = 1 << 18;
+        const Mode3 = 1 << 19;
+        const Mode4 = 1 << 20;
+        const Mode5 = 1 << 21;
+        const Mode6 = 1 << 22;
+        const Mode7 = 1 << 23;
+        const Mode8 = 1 << 24;
+        const Mode9 = 1 << 25;
+        const ModeA = 1 << 26;
+        const ModeB = 1 << 27;
+        const ModeC = 1 << 28;
+        const ModeD = 1 << 29;
+        const ModeE = 1 << 30;
+        const ModeF = 1 << 31;
 
-    /// Mode bit flags,
-    ///
-    pub struct Mode: u16 {
+        /// Load value operation,
+        /// 
+        const Load = Self::Op0.bits();
+        /// Load value by replacing existing value,
+        /// 
+        const Replace = Self::Load.bits() | Self::Mode0.bits();
+        /// Load value by parsing input data as string input,
+        /// 
+        const Parse = Self::Load.bits() | Self::Mode1.bits();
+        /// Load value by merging input data,
+        /// 
+        const Merge = Self::Load.bits() | Self::Mode2.bits();
+        /// Load value by appending input data,
+        /// 
+        const Append = Self::Load.bits() | Self::Mode3.bits();
 
+        /// Read value operation,
+        /// 
+        const Read = Self::Op1.bits();
+        /// Read value as a bincode utf8 string,
+        /// 
+        const String = Self::Op1.bits() | Self::Mode0.bits();
+        /// Read value as a bincode Decorated<String>,
+        /// 
+        const DecoratedString = Self::Op1.bits() | Self::Mode1.bits();
+
+        /// Util operation,
+        /// 
+        const Util = Self::Op2.bits();
+        
     }
 }
