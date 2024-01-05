8f0d3f86f668319987b22019147dca7d79bb2759
Julius <juliusl@microsoft.com>
2024-01-04 16:43:14 -0800

    todo. starting project system commands

---
+++ nebudeck/lib/runmd/commands.md
@@ -0,0 +1,20 @@
+
+```runmd
+# -- Install commands for a Rust Project 
++ .operation install
+
+# -- Install nebudeck as a dependency of the current project
+<nebudeck/builtin.process> cargo
+: .arg  install
+: .arg  nebudeck
+
+# -- Install loopio as a dependency of the current project
+<loopio/builtin.process> cargo
+: .arg  install
+: .arg  loopio
+
+# -- Install commands for a Rust Workspace
++ workspace .operation install
+
+
+```
--- nebudeck/src/nebudeck.rs
+++ nebudeck/src/nebudeck.rs
@@ -1,6 +1,7 @@
 use std::path::PathBuf;
 
 use loopio::prelude::Package;
+use tracing::info;
 
 /// Entrypoint for interacting with a nebudeck project workspace,
 ///
@@ -16,6 +17,18 @@ impl Nebudeck {
     pub fn init(home_dir: PathBuf) -> anyhow::Result<Self> {
         // The home directory being initialized must already exist and be available
         let home_dir = home_dir.canonicalize()?;
+
+        let lib_runmd = home_dir.join("lib/runmd/");
+
+        // Create 
+        if !lib_runmd.exists() {
+            info!("Creating lib/runmd directory");
+            std::fs::create_dir_all(lib_runmd)?;
+        } else {
+            info!("Found lib/runmd");
+        }
+
+
         todo!()
     }
 
